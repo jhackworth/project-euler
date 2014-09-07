ff33b061e0239b326f367b1437bd4277db510bf2
Jonathan Hackworth <jd.hackworth@me.com>
2014-09-07 13:10:12 -0500

    math min function

---
+++ ejs-mathMin.js
@@ -0,0 +1,11 @@
+// Eloquent JavaScript
+// Write a function that takes two arguments and returns their minimum
+
+function min(x,y){
+  return Math.min(x,y);
+}
+
+console.log(min(0, 10));
+// → 0
+console.log(min(0, -10));
+// → -10
